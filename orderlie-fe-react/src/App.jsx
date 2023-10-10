e84f15c8dabdd29ef6932301adc2c6922253994d
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-10-10 15:02:25 +0100

    Created Vitejs and tailwind setup for Orderlie

---
+++ orderlie-fe-react/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-black'> Orderlie-fe-react</h1>
+    </>
+  )
+}
+
+export default App
