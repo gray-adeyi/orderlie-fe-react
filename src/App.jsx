2bf5657614996021341d94c06032a523236be293
Glory135 <79275377+Glory135@users.noreply.github.com>
2023-10-12 13:08:26 +0100

    created home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import './App.css'
-
 import Header from './component/Header'
-import WhatWeOffer from './component/offer/WhatWeOffer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
 
@@ -10,18 +8,7 @@ function App() {
   return (
     <div className='w-screen h-auto flex flex-auto flex-col'>
       <Header />
-      <div className='w-screen h-auto items-center'>
-        <WhatWeOffer />
-        <section className="ready-for-dev-container flex justify-center  py-20 bg-bgColorLight">
-          <div className="ready-for-dev flex flex-col text-center">
-            <h3>Ready to try different work experience now?</h3>
-            <p>Get the best  working experience that you never feel  before</p>
-            <div className='flex justify-center'>
-              <button className='bg-bgColor'>SIGN UP NOW</button>
-            </div>
-          </div>
-        </section>
-      </div>
+      <Outlet />
     </div>
   )
 }
