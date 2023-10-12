cf0294efab34c2c3360f6b775008424dd4389a96
nobledeveloper01 <nobleconcepts22@gmail.com>
2023-10-12 01:27:41 +0100

    Created the Footer section of the home page and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
 
-import Header from './component/Header'
+import Header from "./component/Header";
+import Footer from "./component/footer";
 
 function App() {
- 
-
   return (
-    <div className='w-screen h-auto flex flex-auto '>
-      <Header/>
-
-    </div>
-  )
+    <>
+      <div className="w-screen h-auto flex flex-auto ">
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
